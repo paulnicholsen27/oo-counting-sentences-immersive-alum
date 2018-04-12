3a22ba698af290e9646899051cbc079f411b9d68
paul.nicholsen <paul.nicholsen@flatironschool.com>
2018-04-12 17:28:05 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+  	self[-1] == "?"
   end
 
   def exclamation?
-
+  	self[-1] == "!"
   end
 
   def count_sentences
-
+  	self.split(/\?|\!|\./).reject {|x| x.length < 2}.length
   end
 end
+
